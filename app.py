5c7c367120df418ab1c1e2522796b459e720e5e5
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-08-19 23:04:39 +0900

    main.html & each book page

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, session, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.secret_key = 'sample_secret_key'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost:3306/books_db" 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+migrate.init_app(app, db)
+
+# from . import models 하면 에러나는 이유는?
+from models import *
+
+@app.route("/")
+def hello():
+	
+	if 'log_in' in session:
+		return render_template('main.html')
+	else:
+		return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def login():
+	
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		data = Users.query.filter_by(username =username, password =password).first()
+		
+		if data is not None :
+			session['log_in'] = True
+			session['username'] = username
+			books_info = Books.query.all()
+			# 출력이 원래 int 값만 나오나요??
+			print(books_info)
+			db.session.close()
+			return render_template('main.html', books_info = books_info)
+			
+		return '''<script>alert('nonono'); location.href= '/';</script>'''
+
+@app.route("/logout")
+def logout():
+	session.pop('log_in', None)
+	return redirect(url_for('hello'))
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+
+	if request.method =='POST':
+		username = request.form['username']
+		email = request.form['email']
+		password = request.form['password']
+		check = request.form['check']
+		query = Users(username, email, password)
+
+		if password == check and len(username) != 0 and len(email) != 0 and len(password) != 0:	
+			db.session.add(query)
+			db.session.commit()
+			db.session.close()
+			return render_template('login.html')
+
+	return 	render_template('register.html')
+
+@app.route("/book-info/<bookId>")
+def info(bookId):
+	books_info = Books.query.filter(Books.id == bookId).all()
+	print(books_info)
+	return render_template('base.html', books_info=books_info)
+
+if __name__ == "__main__":
+
+	app.run(debug=True)
--- models.py
+++ models.py
@@ -1,10 +1,29 @@
-from db_connect import db
+from app import db
 
-class Member(db.Model):
+class Users(db.Model):
+    
+    __tablename__ = 'customers'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.Text(), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    # 초기화가 필요한지, 아래에 있는 클래스처럼 안쓰는게 맞는건지?
+    def __init__(self, username, email, password ):
+        self.username = username
+        self.email = email
+        self.password = password
+
+class Books(db.Model):
+    __tablename__ = 'books_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.Text(), nullable=False)
+    publisher = db.Column(db.Text(), nullable=False)
+    author = db.Column(db.String(20), nullable=False)
+    publication_date = db.Column(db.Date, nullable=False)
+    pages = db.Column(db.Integer, primary_key=False)
+    isbn = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    link = db.Column(db.Text(), nullable=False)
