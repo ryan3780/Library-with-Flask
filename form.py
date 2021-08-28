1c74e30f349c0128c878737ef7e6a63f90d05d3a
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-08-28 10:07:32 +0900

    email vaildate add

--- form.py
+++ form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import PasswordField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Email
 
 class RegisterForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired(), EqualTo('password_2')])
     password_2 = PasswordField('repassword', validators=[DataRequired()])
 
